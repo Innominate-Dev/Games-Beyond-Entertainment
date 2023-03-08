8ddd9c5d4dc7ec2ea0f954280ffcefdce49c0c1f
Innominate-Dev <InnominateDev@gmail.com>
2023-03-08 23:45:39 +0000

    Added Player Controller
    - Added Player Controller Script.
    - Added Camera Controller Script.
    - Added Ground Tag.
    
    - Updated glitch where the head bobbing would happen mid air.

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float MouseSensitivity = 10f;
+
+    public Transform playerObj;
+
+    float xRotation = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * MouseSensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * MouseSensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerObj.Rotate(Vector3.up * mouseX);
+
+    }
+}
+
