330e7c7b2ac6d521ceafaa9e2c2c9d91108a9157
Innominate-Dev <InnominateDev@gmail.com>
2023-05-23 12:46:00 +0100

    Added Enemy Patrol
    - Added Enemy Patrol
    - Added Waypoints

--- Assets/Scripts/Cabinet.cs
+++ Assets/Scripts/Cabinet.cs
@@ -34,7 +34,6 @@ public class Cabinet : MonoBehaviour
             }
         }
 
-        Debug.Log(transform.localPosition);
     }
 
     public bool IsCabinetOpen()
--- Assets/Scripts/EnemyPatrol.cs
+++ Assets/Scripts/EnemyPatrol.cs
@@ -7,8 +7,9 @@ public class EnemyPatrol : MonoBehaviour
 
     public List<GameObject> waypoints;
     public float speed = 2;
-    int index = 0;
+    public int index = 0;
     public bool isLoop = true;
+    public float timeCount;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,16 +19,19 @@ public class EnemyPatrol : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
         Vector3 destination = waypoints[index].transform.position;
         Vector3 newPos = Vector3.MoveTowards(transform.position, destination,speed * Time.deltaTime);
         transform.position = newPos;
 
         float distance = Vector3.Distance(transform.position, destination);
-        if(distance <= 0.05)
+        Debug.Log(distance);
+        if(distance <= 0.7f)
         {
+            Debug.Log("CloseBy");
             if(index < waypoints.Count - 1)
             {
+                //transform.Rotate(waypoints[index].transform.position.x, transform.position.y, waypoints[index].transform.position.z);
                 index++;
             }
             else
@@ -36,8 +40,9 @@ public class EnemyPatrol : MonoBehaviour
                 {
                     index = 0;
                 }
-                
+                Debug.Log("Finished at destination");
             }
         }
+
     }
 }
